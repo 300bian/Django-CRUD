97b5b2e3c1c77f45e782ca36233c98625f84a39b
300bian <tudududumax@gmail.com>
2024-05-21 16:43:39 +0800

    Update urls.py
    update change profile function

--- mydrfproject/accounts/urls.py
+++ mydrfproject/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import register_user, user_login, user_logout, change_password,LoginWithOTP, ValidateOTP
+from .views import register_user, user_login, user_logout, change_password,change_profile, LoginWithOTP, ValidateOTP
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('change_password/', change_password, name='change_password'),
+    path('change_profile/', change_profile, name='change_profile'),
     path('login-with-otp/', LoginWithOTP.as_view(), name='login-with-otp'),
     path('validate-otp/', ValidateOTP.as_view(), name='validate-otp'),
 ]
