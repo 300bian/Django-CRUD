085c44c601d0266403a618b66318789fdef82b7d
300bian <tudududumax@gmail.com>
2024-05-21 16:42:56 +0800

    Update serializers.py
    update change profile function

--- mydrfproject/accounts/serializers.py
+++ mydrfproject/accounts/serializers.py
@@ -1,13 +1,10 @@
 from rest_framework import serializers
 from .models import CustomUser
-from django.contrib.auth import get_user_model
-
-User = get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password','first_name','last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -19,6 +16,25 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+class ChangeProfileSerializer(serializers.Serializer):
+    new_username = serializers.CharField(required=False,allow_blank=True)
+    new_email = serializers.CharField(required=False,allow_blank=True)
+    new_first_name = serializers.CharField(required=False,allow_blank=True)
+    new_last_name = serializers.CharField(required=False,allow_blank=True)
+
+    def update(self, instance, validated_data):
+        if 'new_username' in validated_data:
+            instance.username = validated_data['new_username']
+        if 'new_email' in validated_data:
+            instance.email = validated_data['new_email']
+        if 'new_first_name' in validated_data:
+            instance.first_name = validated_data['new_first_name']
+        if 'new_last_name' in validated_data:
+            instance.last_name = validated_data['new_last_name']
+        
+        instance.save()
+        return instance
+    
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
